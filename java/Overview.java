71cc14baaa53a54e920d6e028209af45210761c5
JacksonMowry <jacksonm@duck.com>
2022-01-31 18:24:12 -0500

    jarch

---
+++ java/Overview.java
@@ -0,0 +1,12 @@
+// This program will *not* find the distance between two points
+// Jackson Mowry
+// <2022-01-25 Tue>
+
+import java.util.Scanner;
+class test {
+    public static void main(String[] args) {
+        int var1 = 100;
+        System.out.println(var1);
+       
+    }
+}
