e0907a58b69db6de97de3f9b164c222a4846635e
JingerJack <tryhardsax@gmail.com>
2022-02-08 12:46:58 -0500

    y

--- cs101/arithmetic.java
+++ cs101/arithmetic.java
@@ -1,6 +1,7 @@
 class Arithmetic {
     public static void main(String[] args) {
         // Code Here
-
+        System.out.println(3 + 6);
+        System.out.println(3 + 6 - 2 * 4 + 5);
     }
 }
